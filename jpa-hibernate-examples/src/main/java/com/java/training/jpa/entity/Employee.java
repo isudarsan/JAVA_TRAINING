b49c263c5d9244f677f31173991dfc8e8c103904
Sudarshan A <Ambatipudi.Sudarsan@nissan-usa.com>
2022-02-25 11:06:42 +0530

    Added JPA Project

---
+++ jpa-hibernate-examples/src/main/java/com/java/training/jpa/entity/Employee.java
@@ -0,0 +1,71 @@
+package com.java.training.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	private int employeeId;
+
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "age")
+	private int age;
+	
+	@Column(name = "salary")
+	private double salary;
+
+	// Always provide default constructor in Entity class.
+	public Employee() {
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
